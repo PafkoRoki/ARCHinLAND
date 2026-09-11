6a78716a3fcd7ed1ef42ccff4307b8c7271b88a7
PafkoRoki <pafko.roki@gmail.com>
2026-09-11 05:18:56 +0200

    tt

--- frontend/src/components/2_About.tsx
+++ frontend/src/components/2_About.tsx
@@ -1,29 +1,7 @@
 
-import x1 from '../assets/Projects/1.jpg';
-import x2 from '../assets/Projects/2.jpg';
-import x3 from '../assets/Projects/3.jpg';
-import x4 from '../assets/Projects/4.jpg';
 import MagicBento from './MagicBento'
 import "./2_About.css";
 
-const items = [
-  {
-    image: x1,
-    caption: 'ARCHITECTURE in LAND DEVELOPMENT'
-  },
-  {
-    image: x2,
-    caption: "Biuro prowadzone przez Andrzeja Kurkę",
-  },
-  {
-    image: x3,
-    caption: 'Od 30 lat łączymy projektowanie architektoniczne z wiedzą techniczną'
-  },
-  {
-    image: x4,
-    caption: '˚ ✶ ⋆｡˚ ➷'
-  }
-];
 
 const About = () => {
   return (
